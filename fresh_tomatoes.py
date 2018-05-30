ff2fd1af9e347c913d08f83046e96d0e8aaba460
dalia-maher <dalia.maher94@gmail.com>
2018-05-30 08:26:32 +0200

    modifying header tag in movie poster

--- fresh_tomatoes.py
+++ fresh_tomatoes.py
@@ -122,7 +122,6 @@ movie_tile_content = '''
 <div class="col-md-6 col-lg-4 movie-tile text-center" data-trailer-youtube-id="{trailer_youtube_id}" data-toggle="modal" data-target="#trailer">
     <img src="{poster_image_url}" width="220" height="342">
     <h3>{movie_title} ({release_year})</h3>
-	<h3>
 </div>
 '''
 
